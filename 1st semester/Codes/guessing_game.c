e2bd791e13517c129cb035f1531be5dc59a78f99
Chrono <01914rifat@tuta.io>
2022-06-21 12:43:32 +0600

    Edit guessing_game.c

--- 1st semester/Codes/guessing_game.c
+++ 1st semester/Codes/guessing_game.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
-int game(int x);
+void game(int);
+
 int main()
 {
-    int   
+    int x;
+    printf("Guess the number: \n");
+    scanf("%d", &x); // taking the guessed number from user
+    game(x);
     return 0;
+}
+
+void game(int x)
+{
+    int y = 96; // stored number
+    if (x == y)
+    {
+        printf("Congratulations! You have won the game\n");
+    }
+    else if (x > y)
+    {
+        printf("Wrong guess! Too High!\n");
+    }
+    else if (x < y)
+    {
+        printf("Wrong guess! Too Low!\n");
+    }
 }
