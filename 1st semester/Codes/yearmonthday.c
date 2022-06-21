40793e9eeec0caa6cd28967e878d8785e1f75327
Chrono <01914rifat@tuta.io>
2022-06-21 17:50:12 +0600

    Edit yearmonthday

--- 1st semester/Codes/yearmonthday.c
+++ 1st semester/Codes/yearmonthday.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main()
 {
     float n,y,m,d;
@@ -7,4 +8,5 @@ int main()
     m = y * 12.0;
     d = y * 365.0;
     printf("%.3f years\n%.3f months\n%.3f days\n",y,m,d);
+
 }
