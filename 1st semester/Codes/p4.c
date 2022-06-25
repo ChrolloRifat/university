fdf1bc774b8d5a855f8733c282bb49cb9059ea30
Chrono <01914rifat@tuta.io>
2022-06-25 20:58:56 +0600

    Edit p4.c

--- 1st semester/Codes/p4.c
+++ 1st semester/Codes/p4.c
@@ -21,43 +21,39 @@ int main()
         notes1000 = amt / 1000;
         amt = amt - (notes1000 * 1000);
     }
-    else if (amt >= 500)
+    if (amt >= 500)
     {
         notes500 = amt / 500;
         amt = amt - (notes500 * 500);
     }
-    else if (amt >= 100)
+    if (amt >= 100)
     {
         notes100 = amt / 100;
         amt = amt - (notes100 * 100);
     }
-    else if (amt >= 50)
+    if (amt >= 50)
     {
         notes50 = amt / 50;
         amt = amt - (notes50 * 50);
     }
-    else if (amt >= 20)
+    if (amt >= 20)
     {
         notes20 = amt / 20;
         amt = amt - (notes20 * 20);
     }
-    else if (amt >= 10)
+    if (amt >= 10)
     {
         notes10 = amt / 10;
         amt = amt - (notes10 * 10);
     }
-    else if (amt >= 5)
+    if (amt >= 5)
     {
         notes5 = amt / 5;
         amt = amt - (notes5 * 5);
     }
-    else if (amt >= 2)
+    if (amt >= 2)
     {
         notes2 = amt;
-    }
-    else
-    {
-        printf("Invalid amount\n");
     }
 
     printf("\nAmount of BDT1000 notes = %d\n", notes1000);
