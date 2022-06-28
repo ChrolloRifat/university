d824e40b57b507f1621abb25103881ef940e670a
Chrono <01914rifat@tuta.io>
2022-06-28 23:41:43 +0600

    Add labquiz1.c

---
+++ 1st semester/Codes/labquiz1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+char generateGrade(float,float,float,float);
+int main()
+{
+    float q,m,f,a;
+    char grade;
+    scanf("%f%f%f%f",&q,&m,&f,&a);
+    grade = generateGrade(q,m,f,a);
+    printf("Your grade is %c\n",grade);
+
+}
+char generateGrade(float quiz, float mid, float fin, float assign)
+{
+    char gra;
+    float sum = (0.2 * quiz) + (0.3 * mid) + (0.4 * fin) + (0.1 * assign);
+    if(sum >= 90)
+    {
+        gra = 'A';
+    }
+    else if(sum >= 80 && sum < 90)
+    {
+        gra = 'B';
+    }
+    else if(sum >= 70 && sum < 80)
+    {
+        gra = 'C';
+    }
+    else if(sum >= 60 && sum < 70)
+    {
+        gra = 'D';
+    }
+    else
+    {
+        gra = 'FAIL';
+    }
+    return gra;
+}
