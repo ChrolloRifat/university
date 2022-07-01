67ff87d2de49c2d7751fc784c8a9cdd958a1779d
Chrono <01914rifat@tuta.io>
2022-07-01 23:44:42 +0600

    Edit series4.c

--- 1st semester/Codes/series4.c
+++ 1st semester/Codes/series4.c
@@ -1,31 +1,22 @@
 /**
- * C program to compute the series
+ * C program to compute the series 1/1-1/3+1/5-...
  */
 #include <stdio.h>
 #include <math.h>
 int main()
 {
-    int i, count = 0;
-    float sum = 0;
-    i = 1;
-    count = 2;
+    int count = 1;
+    int n = 3;
+    float total, pos, neg, sum = 0;
     // printf("Please enter the value of n: ");
     // scanf("%f", &n);
-    while (count <= 10)
+    while (count <= 5 && n <= 5)
     {
-        if (i % 2 == 1)
-        {
-            sum = sum - i / count;
-            count++;
-            i++;
-        }
-        else
-        {
-            sum = sum + i / count;
-            count++;
-            i++;
-        }
+        sum = sum + (1 / count);
+        total = sum - (1 / n);
+        count = count + 2;
+        n = n + 4;
     }
-    printf("sum = %.3f\n", sum);
+    printf("sum = %.3f\n", total);
     return 0;
 }
