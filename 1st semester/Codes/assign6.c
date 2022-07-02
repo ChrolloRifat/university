62045e011d6a832c8660e48a4f52f356d766d935
Chrono <01914rifat@tuta.io>
2022-07-02 21:13:14 +0600

    Edit assign6.c

--- 1st semester/Codes/assign6.c
+++ 1st semester/Codes/assign6.c
@@ -2,21 +2,26 @@
  *C program to find power of any number
  */
 #include <stdio.h>
+int perm(int n)
+{
+    int i, result = 1;
+    for (i = 2; i <= n; i = i + 1)
+    {
+        result = result * i;
+    }
+    return result;
+}
 int main()
 {
-    int n, r, i, npr;
+    int n, r, npr;
 
     printf("Enter n:");
     scanf("%d", &n);
     printf("Enter r: ");
     scanf("%d", &r);
-    int result = n;
-    for (i = 1; i <= r; i = i + 1)
-    {
-        result = result * (n - i);
-    }
 
-    printf("%d \n", result);
+    npr = perm(n) / perm(n - r);
+    printf("%dP%d = %d\n", n, r, npr);
 
     return 0;
 }
